1d1d74057ae122fa262832d59b619f83d87a657b
Marsel Gaisin <marselgaisin@gmail.com>
2025-09-29 19:22:17 +0300

    дз 1

---
+++ src/main/java/com/algo/bullet/BinaryTreeMaxDepth.java
@@ -0,0 +1,38 @@
+package com.algo.bullet;
+
+public class BinaryTreeMaxDepth {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(
+                3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7))
+        );
+
+        System.out.println(maxDepth(root)); // ожидаем 3
+    }
+}
